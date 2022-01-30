dad43429d1094cb882e8f08ae53849fa1797b097
JimmyVoRepay <77993754+JimmyVoRepay@users.noreply.github.com>
2022-01-30 17:30:41 -0500

    Update schema to remove TimeSlot table

--- Databases/Tables/Customers.sql
+++ Databases/Tables/Customers.sql
@@ -1,6 +1,7 @@
 Create Table Customers(
 	CustomerId int IDENTITY(1,1),
 	CustomerName NVARCHAR(100) NOT NULL,
-	EmailAddress NVARCHAR(100) NOT NULL,
+	EmailAddress NVARCHAR(100) NOT NULL UNIQUE,
 	CONSTRAINT PK_CustomerId PRIMARY KEY CLUSTERED (CustomerId)
+	--Jimmy Potentailly create an index for email and name
 );
--- Databases/Tables/Reservations.sql
+++ Databases/Tables/Reservations.sql
@@ -2,8 +2,10 @@ Create Table Reservations(
 	ReservationId int IDENTITY(1,1),
 	TimeslotId int NOT NULL,
 	PartySize int NOT NULL, 
-	CustomerId int NULL, 
+	CustomerId int NULL,
+	RestaurantId int NOT NULL,
+	TimeSlotSection tinyint NOT NULL, 
 	CONSTRAINT PK_ReservationId PRIMARY KEY CLUSTERED (ReservationId),
-	CONSTRAINT FK_Reservations_Timeslots FOREIGN KEY (TimeslotId) REFERENCES Timeslots(TimeslotId),
+	CONSTRAINT FK_Reservations_Restaurants FOREIGN KEY (RestaurantId) REFERENCES Restaurants(RestaurantId),
 	CONSTRAINT FK_Reservations_Customers FOREIGN KEY (CustomerId) REFERENCES Customers(CustomerId)
 );
